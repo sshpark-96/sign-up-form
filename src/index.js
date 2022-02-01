19769e11ede0bde54c9b826eac28c28e25b2625f
Scott Park <scott.park.dev@gmail.com>
2022-01-31 18:00:35 -0800

    Made further changes to the page

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
+import "./css/default.css";
+import "./css/index.css";
+import image from "./img/Not Sketchy-logos_white.png"
+
 console.log("Hello World");
+
+document.querySelector(".logo").setAttribute("src", image);
