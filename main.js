41ec5231e0759248a1247a6e8c53d18eb740f0f2
Scott Park <scott.park.dev@gmail.com>
2022-01-31 16:56:12 -0800

    Set up initial for the form page

---
+++ main.js
@@ -0,0 +1,3 @@
+/*! For license information please see main.js.LICENSE.txt */
+console.log("Hello World");
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5qcyIsIm1hcHBpbmdzIjoiO0FBQUFBLFFBQVFDLElBQUkiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9zaWduLXVwLWZvcm0vLi9zcmMvaW5kZXguanMiXSwic291cmNlc0NvbnRlbnQiOlsiY29uc29sZS5sb2coXCJIZWxsbyBXb3JsZFwiKTsiXSwibmFtZXMiOlsiY29uc29sZSIsImxvZyJdLCJzb3VyY2VSb290IjoiIn0=
